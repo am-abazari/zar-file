e73bba714724957ebdaef2ca5d2c3a721df7c3f5
Amirhossein <amirhossein.abazari@yahoo.com>
2026-01-26 22:54:17 +0330

    [ADD] Footer

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+//components
+import CircleShadow from "@components/CircleShadow";
+
+//icons
+import SolarProgrammingBroken from "@icons/SolarProgrammingBroken";
+
+const Footer = () => {
+    return (
+        <>
+            <CircleShadow size={"small"} className={`mt-20 bg-yellow-300 -bottom-50 right-0 left-0 mx-auto`} />
+            <footer className="mt-25 w-full p-8 bg-white/5 backdrop-blur-2xl shadow-xl rounded-2xl mb-3 sm:text-base text-sm">
+                <p className={"flex  items-center gap-3"}><SolarProgrammingBroken /> Developed by <Link target={"_blank"} className={"text-(--text-primary) font-semibold"} href={"https://www.amabazari.ir"}>Amirhossein Abazari</Link></p>
+            </footer>
+        </>
+    )
+}
+
+
+export default Footer
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,10 @@
 // import { Inter } from "next/font/google";
+
+// styles
 import "./globals.css";
+
+//components
+import Footer from "@components/Footer";
 
 // const inter = Inter({
 // variable: "--font-inter",
@@ -15,7 +20,8 @@ export default function RootLayout({children}) {
     return (
         <html lang="en">
         <body className={``}>
-            {children}
+        {children}
+        <Footer />
         </body>
         </html>
     );
