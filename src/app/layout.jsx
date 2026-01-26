529b800fff61dcf5e7bc2c7b24151913c710cea9
Amirhossein <amirhossein.abazari@yahoo.com>
2026-01-26 18:46:45 +0330

    [DELETE] font Inter for temp

---
+++ src/app/layout.jsx
@@ -0,0 +1,22 @@
+// import { Inter } from "next/font/google";
+import "./globals.css";
+
+// const inter = Inter({
+// variable: "--font-inter",
+// subsets: ["latin"],
+// });
+
+export const metadata = {
+    title: "Zar File | Home",
+    description: "Platform for Downloading and Uploading Files",
+};
+
+export default function RootLayout({children}) {
+    return (
+        <html lang="en">
+        <body className={` antialiased`}>
+            {children}
+        </body>
+        </html>
+    );
+}
