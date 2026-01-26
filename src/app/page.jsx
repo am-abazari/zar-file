82d7012d9f55d0a2b80afdb3160f8537df7db010
Amirhossein <amirhossein.abazari@yahoo.com>
2026-01-27 00:14:07 +0330

    [ADD] HERO component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,3 @@
-
 // Components
 import CircleShadow from "@components/CircleShadow";
 import Button from "@components/Button";
@@ -12,26 +11,28 @@ import MaterialSymbolsFolderSpecialRounded from "@icons/MaterialSymbolsFolderSpe
 import FluentNote16Filled from "@icons/FluentNote16Filled";
 import MaterialSymbolsCloudLockRounded from "@icons/MaterialSymbolsCloudLockRounded";
 import SolarGallerySendLineDuotone from "@icons/SolarGallerySendLineDuotone";
+import Hero from "./components/Hero/Hero";
 
 export default function Home() {
     return (
-        <div className={"pt-10 relative text-center"}>
-            <h1 className={`heading mt-10 font-semibold`}>The <span
+        <div>
+            <Hero title={<>The <span
                 className={`text-(--text-primary) font-bold`}>Open-Source</span> <br/> Platform for Uploading and
-                Downloading Files</h1>
-            <CircleShadow className={`inset-x-0 m-auto -mt-18 bg-(--color-primary)`}/>
-            <p className={`text-(--text-secondary) mt-10`}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Downloading Files</>}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                 Accusantium asperiores autem commodi
                 consequuntur, officiis pariatur velit! Adipisci aliquam aliquid aperiam dolorem, excepturi minus
                 provident repudiandae saepe unde vero. Ad commodi cumque dignissimos dolorem, in libero minus mollitia,
-                natus officia, perferendis praesentium quae quas quidem quo saepe sit unde. Atque, cumque?</p>
+                natus officia, perferendis praesentium quae quas quidem quo saepe sit unde. Atque, cumque?
+                <div className="flex gap-6 justify-center items-center mt-16 flex-wrap">
+                    <Button type={"link"} href={"/file"} className={'basis-66'} variant={"fill"}
+                            icon={<ZondiconsBolt/>}>Start
+                        Using Zar File</Button>
+                    <Button type={"link"} href={"https://www.github.com/am-abazari/zar-file"} target={"_blank"}
+                            className={'basis-48'} variant={"border"} icon={<GrommetIconsGithub/>}>Github</Button>
+                </div>
+            </Hero>
 
-            <div className="flex gap-6 justify-center items-center mt-16 flex-wrap">
-                <Button type={"link"} href={"/file"} className={'basis-66'} variant={"fill"} icon={<ZondiconsBolt/>}>Start
-                    Using Zar File</Button>
-                <Button type={"link"} href={"https://www.github.com/am-abazari/zar-file"} target={"_blank"}
-                        className={'basis-48'} variant={"border"} icon={<GrommetIconsGithub/>}>Github</Button>
-            </div>
 
             <div className="flex flex-wrap gap-5 mt-36 ">
                 <CircleShadow size={"big"} className={`mt-36 bg-(--color-primary-light)`}/>
@@ -70,7 +71,8 @@ export default function Home() {
                         </p>
                     </div>
                 </Card>
-                <Card icon={<SolarGallerySendLineDuotone />} title={<>Private <span className='font-thin text-sm'>&</span> Public Share</>}>
+                <Card icon={<SolarGallerySendLineDuotone/>}
+                      title={<>Private <span className='font-thin text-sm'>&</span> Public Share</>}>
                     <div>
                         <p>Lorem ipsum dolor sit amet,
                             consectetur adipisicing elit. Atque, consequuntur, voluptatem. Aut laboriosam quas sed.
