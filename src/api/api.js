87a01086580809ab1d3aa3d59bdcd1dde6df4c06
Amirhossein <amirhossein.abazari@yahoo.com>
2026-01-30 19:31:47 +0330

    [UPDATE] axios config base url

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,14 @@
 import axios from "axios";
 
+const API_URL = process.env.REACT_APP_API_URL
+  ? process.env.REACT_APP_API_URL
+  : "/api";
+const API_VERSION = process.env.REACT_APP_API_VERSION
+  ? process.env.REACT_APP_API_VERSION
+  : "v1";
+
 const api = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/${process.env.NEXT_PUBLIC_API_VERSION}`,
+  baseURL: `${API_URL}/${API_VERSION}`,
 });
 
 export default api;
