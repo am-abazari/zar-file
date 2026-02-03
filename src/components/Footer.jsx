894a0686479106916c6997f2c010d79879c8a81e
Amirhossein <amirhossein.abazari@yahoo.com>
2026-02-03 14:45:42 +0330

    [FIX] footer position to relative to toggle the circle shadow to top

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ import SolarProgrammingBroken from "@icons/SolarProgrammingBroken";
 const Footer = () => {
   return (
     <>
-      <footer className="mt-15 w-full p-8 bg-(--color-white-blur) backdrop-blur-2xl shadow-xl rounded-2xl mb-3 sm:text-base text-sm">
+      <footer className="mt-15 w-full relative p-8 bg-(--color-white-blur) backdrop-blur-2xl shadow-xl rounded-2xl mb-3 sm:text-base text-sm">
         <CircleShadow
           size={"small"}
           className={`mt-20 bg-yellow-300 bottom-0 right-0 left-0 mx-auto`}
