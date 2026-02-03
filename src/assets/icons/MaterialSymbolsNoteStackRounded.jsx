575d375d5d03e01057fd2cc00825919df1c878f7
Amirhossein <amirhossein.abazari@yahoo.com>
2026-02-03 21:27:14 +0330

    [FIX] double export icon

--- src/assets/icons/MaterialSymbolsNoteStackRounded.jsx
+++ src/assets/icons/MaterialSymbolsNoteStackRounded.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export function MaterialSymbolsNoteStackRounded(props) {
+function MaterialSymbolsNoteStackRounded(props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
